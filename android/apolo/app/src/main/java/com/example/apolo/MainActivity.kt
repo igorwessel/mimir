3459e63ea4cbb7de7d6c2f4fbf5e782f99d3e549
Igor Wessel <igor_wessel@hotmail.com>
2022-11-25 07:26:18 -0300

    feat: simple layout for a music player

---
+++ android/apolo/app/src/main/java/com/example/apolo/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.apolo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportActionBar?.hide()
+
+        setContentView(R.layout.activity_main)
+    }
+}
